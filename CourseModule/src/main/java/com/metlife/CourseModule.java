e67cbe97f72f358fc6f71aa5360ac40720eb98c7
Jadu hai <Jadu@example.com>
2024-03-15 18:33:42 +0530

    first commit

---
+++ CourseModule/src/main/java/com/metlife/CourseModule.java
@@ -0,0 +1,13 @@
+package com.metlife;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseModule {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CourseModule.class, args);
+	}
+
+}
